38de1b846cb8d2e3eced023d21051cb94ed9ac35
dollar007 <vlad.karapetyan.1988@mail.ru>
2015-11-12 20:25:41 +0400

    Configuration oxd-php

---
+++ Oxd_config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Vlad
+ * Date: 11/12/2015
+ * Time: 6:27 PM
+ */
+class Oxd_config
+{
+    public static $localhostIp;
+    public static $localhostPort;
+    public static $gluuServerUrl;
+    public static $amHost;
+    public static $userId;
+    public static $userSecret;
+    public static $clientId;
+    public static $clientSecret;
+    public static $clientRedirectURL;
+    public static $logoutRedirectUrl;
+    public static $discoveryUrl;
+    public static $umaDiscoveryUrl;
+    public static $appType;
+}
